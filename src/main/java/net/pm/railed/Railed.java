bc0f2fcd5eca9b990c01357af550fb21dd0a1d54
PaulMarmet <paulmarmet1002@gmail.com>
2024-08-14 22:51:03 -0400

    Initial Commit

---
+++ src/main/java/net/pm/railed/Railed.java
@@ -0,0 +1,15 @@
+package net.pm.railed;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Railed implements ModInitializer {
+	public static final String MOD_ID = "railed";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
